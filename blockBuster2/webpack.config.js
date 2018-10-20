60c1ebce5b7bb0c067b2ff8e051a8e4b6ba4f1a4
Soma Sörös <soma.soros@dpc.hu>
2018-10-20 14:35:48 +0200

    add webpack dev server

--- blockBuster2/webpack.config.js
+++ blockBuster2/webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({title: 'Block BUSTER!'})
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
