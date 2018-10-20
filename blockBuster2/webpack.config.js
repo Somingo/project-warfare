3c9e5ed15615faf9ce05decc52871e68d059c312
Soma Sörös <soma.soros@dpc.hu>
2018-10-20 19:13:24 +0200

    add static assets copy

--- blockBuster2/webpack.config.js
+++ blockBuster2/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -20,6 +21,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({title: 'Block BUSTER!'}),
+        new CopyWebpackPlugin([{ from: 'assets/**/*', to: './', force: true }])
     ],
     output: {
         filename: '[name].bundle.js',
