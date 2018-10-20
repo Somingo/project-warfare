9cece181128e6556ad23fa2954bd9835c32a4082
Soma Sörös <soma.soros@dpc.hu>
2018-10-20 16:53:55 +0200

    use fps meter in main loop

--- blockBuster2/src/Game.ts
+++ blockBuster2/src/Game.ts
@@ -1,24 +1,31 @@
 import {Sprite} from './engine/Sprite';
 import {UpdateEvent} from './engine/UpdateEvent';
+import {FpsMeter} from './engine/FpsMeter';
 
 export class Game implements Sprite {
 
     width: number;
     height: number;
 
+    sprites: Sprite[] = [];
+
     constructor(width: number, height: number) {
         this.width = width;
         this.height = height;
+        this.sprites.push(new FpsMeter());
     }
 
     init(): void {
+        this.sprites.forEach(sprite=>sprite.init());
     }
 
-    update(e: UpdateEvent): void {
+    update(updateEvent: UpdateEvent): void {
+        this.sprites.forEach(sprite=>sprite.update(updateEvent));
     }
 
     draw(ctx: CanvasRenderingContext2D): void {
         ctx.clearRect(0, 0, this.width, this.height);
+        this.sprites.forEach(sprite=>sprite.draw(ctx));
     }
 
 }
