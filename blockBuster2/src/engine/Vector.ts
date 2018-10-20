62c3c19a9c77cd42daea060f7e91759ee9088a84
Soma Sörös <soma.soros@dpc.hu>
2018-10-20 18:04:47 +0200

    improove vector functionality

--- blockBuster2/src/engine/Vector.ts
+++ blockBuster2/src/engine/Vector.ts
@@ -7,7 +7,31 @@ export class Vector {
         this.y = y;
     }
 
+    static getRandomVector():Vector {
+        return new Vector(Math.random(), Math.random());
+    }
+
+    clone(): Vector {
+        return new Vector(this.x,this.y);
+    }
+
     length():number {
         return Math.sqrt(this.x*this.x+this.y*this.y);
+    }
+
+    add(v:Vector):Vector {
+        this.x += v.x;
+        this.y += v.y;
+        return this;
+    }
+
+    divideWithConst(c:number):Vector {
+        this.x /= c;
+        this.y /= c;
+        return this;
+    }
+
+    getUnit():Vector {
+        return this.clone().divideWithConst(this.length());
     }
 }
