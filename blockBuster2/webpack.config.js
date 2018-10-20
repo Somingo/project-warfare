737159443291e7891f06d581fc919b91f30fdb1f
Soma Sörös <soma.soros@dpc.hu>
2018-10-20 14:48:56 +0200

    add webpack hot module replacement

--- blockBuster2/webpack.config.js
+++ blockBuster2/webpack.config.js
@@ -1,19 +1,30 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({title: 'Block BUSTER!'})
-    ],
+    entry: {
+        app: './src/index.js'
+    },
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true
     },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({title: 'Block BUSTER!'}),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    mode: 'development'
 };
