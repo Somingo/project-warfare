af2f4032beac9eb694f7d54abdf5fbe31a69a081
Soma Sörös <aranytestuisten@gmail.com>
2018-12-15 13:52:02 +0100

    improove Game canvas implementation

--- blockBuster2/src/engine/GameCanvas.ts
+++ blockBuster2/src/engine/GameCanvas.ts
@@ -1,20 +1,21 @@
 export class GameCanvas {
 
+    static CANVAS_ID = 'gameCanvas';
+
     static GAME_WIDTH = 800;
     static GAME_HEIGHT = 600;
 
     static getCanvas(width = GameCanvas.GAME_WIDTH, height = GameCanvas.GAME_HEIGHT): any{
         // @ts-ignore
-        let gameCanvas:HTMLCanvasElement = document.getElementById('gameCanvas');
+        let gameCanvas:HTMLCanvasElement = document.getElementById(GameCanvas.CANVAS_ID);
         if (gameCanvas == undefined) {
             // @ts-ignore
             gameCanvas = document.createElement('canvas');
-            gameCanvas.id = 'gameCanvas';
+            gameCanvas.id = GameCanvas.CANVAS_ID;
             gameCanvas.width = width;
             gameCanvas.height = height;
             document.body.appendChild(gameCanvas);
         }
-        console.log('gameCAnvas', gameCanvas);
         return gameCanvas;
     }
 }
