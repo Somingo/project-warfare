ffd6dd2c8e191a25f4ac799e1075ead1d7daefbf
Soma Sörös <aranytestuisten@gmail.com>
2018-12-17 00:20:18 +0100

    add example tile to platformer and create tile set mapping constants

--- blockBuster2/src/platformer/Platformer.ts
+++ blockBuster2/src/platformer/Platformer.ts
@@ -2,17 +2,23 @@ import {Sprite} from '../engine/Sprite';
 import {UpdateEvent} from '../engine/UpdateEvent';
 import {Parallax} from '../engine/parallax/Parallax';
 import {CITY} from './Paralaxes';
+import {TileSet} from '../engine/tile/TileSet';
+import {MAP_TILE_SET} from './MapTileSet';
 
 export class Platformer implements Sprite {
 
     bg: Parallax = new Parallax(CITY.layerOptions);
+    tileSet: TileSet = MAP_TILE_SET;
+    testTile = MAP_TILE_SET.getTile('snowHalfRight', 50, 50);
 
     draw(ctx: CanvasRenderingContext2D): void {
         this.bg.draw(ctx);
+        this.testTile.draw(ctx);
     }
 
     init(): void {
         this.bg.init();
+        this.tileSet.init();
     }
 
     update(e: UpdateEvent): void {
