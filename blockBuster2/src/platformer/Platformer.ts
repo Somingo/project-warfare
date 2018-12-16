d3852f5482383c6d928e569c2e133296079e71f6
Soma Sörös <aranytestuisten@gmail.com>
2018-12-16 22:45:06 +0100

    set parallax background to platformer

--- blockBuster2/src/platformer/Platformer.ts
+++ blockBuster2/src/platformer/Platformer.ts
@@ -1,13 +1,21 @@
 import {Sprite} from '../engine/Sprite';
 import {UpdateEvent} from '../engine/UpdateEvent';
+import {Parallax} from '../engine/parallax/Parallax';
+import {CITY} from './Paralaxes';
 
 export class Platformer implements Sprite {
+
+    bg: Parallax = new Parallax(CITY.layerOptions);
+
     draw(ctx: CanvasRenderingContext2D): void {
+        this.bg.draw(ctx);
     }
 
     init(): void {
+        this.bg.init();
     }
 
     update(e: UpdateEvent): void {
+        this.bg.update(e);
     }
 }
