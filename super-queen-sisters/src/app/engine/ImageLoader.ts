d885e036ca62633c6ffd02ead22074e80e81061e
Soma Sörös <soma.soros@dpc.hu>
2019-05-04 15:35:50 +0200

    add parallax, sprites to SQS, upgrade image loader

--- super-queen-sisters/src/app/engine/ImageLoader.ts
+++ super-queen-sisters/src/app/engine/ImageLoader.ts
@@ -1,8 +1,10 @@
-export class ImageLoader {
-    loadedImages: { path: string, image: HTMLImageElement }[] = [];
+export type LoadedImage =  { path: string, image: HTMLImageElement };
 
-    loadImage(path: string): any {
-        let res = this.loadedImages.filter(x => x.path === path)[0];
+export class ImageLoader {
+    static loadedImages: LoadedImage[] = [];
+
+    static loadImage(path: string): LoadedImage {
+        let res = ImageLoader.loadedImages.filter(x => x.path === path)[0];
         if (res == undefined) {
             const imageToLoad = new Image();
             imageToLoad.src = path;
--- super-queen-sisters/src/app/game/SuperQueenSisters.ts
+++ super-queen-sisters/src/app/game/SuperQueenSisters.ts
@@ -1,17 +1,27 @@
 import {Sprite} from "../engine/Sprite";
 import {UpdateEvent} from "../engine/UpdateEvent";
+// @ts-ignore
+import parallaxes from './parallaxes.json';
+import {Parallax} from "../engine/parallax/Parallax";
+import {ParallaxOptions} from "../engine/parallax/ParallaxOptions";
 
 export class SuperQueenSisters implements Sprite {
-
+  parallax:Parallax = null;
 
   draw(ctx: CanvasRenderingContext2D): void {
+    if (this.parallax) this.parallax.draw(ctx);
     ctx.fillText('Hello World!', 50, 50);
   }
 
   init(): void {
+    console.log('loaded?', parallaxes);
+    let parallaxOptions = ParallaxOptions.fromObject(parallaxes[0]);
+    this.parallax = new Parallax(parallaxOptions.layerOptions);
+    this.parallax.init();
   }
 
   update(e: UpdateEvent): void {
+    if (this.parallax) this.parallax.update(e);
   }
 
 }
