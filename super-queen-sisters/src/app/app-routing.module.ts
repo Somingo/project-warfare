ad98760d404fee7f5eb02c9348234cea5c7e99ba
Soma Sörös <soma.soros@dpc.hu>
2019-05-04 12:01:25 +0200

    create base routing

--- super-queen-sisters/src/app/app-routing.module.ts
+++ super-queen-sisters/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {GameComponent} from "./game/game.component";
+import {MapEditorComponent} from "./map-editor/map-editor.component";
+import {SpriteEditorComponent} from "./sprite-editor/sprite-editor.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'game', component: GameComponent},
+  {path: 'mapEditor', component: MapEditorComponent},
+  {path: 'spriteEditor', component: SpriteEditorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
