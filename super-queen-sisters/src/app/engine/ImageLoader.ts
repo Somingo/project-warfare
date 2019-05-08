d4378d37367ab1068fa3c8d11486b6c04a130708
Soma Sörös <soma.soros@dpc.hu>
2019-05-08 12:18:38 +0200

    implement a proper caching ImageLoader

--- super-queen-sisters/src/app/engine/ImageLoader.ts
+++ super-queen-sisters/src/app/engine/ImageLoader.ts
@@ -12,6 +12,7 @@ export class ImageLoader {
                 path: path,
                 image: imageToLoad
             };
+            ImageLoader.loadedImages.push(res);
         }
         return res.image;
     }
